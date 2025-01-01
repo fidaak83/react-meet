4d9bcab1d31d95a0f398811c4d3eda098fd57e23
fidaak83 <bdcrak-fidaak1983@yahoo.com>
2025-01-02 00:32:09 +0300

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -64,14 +64,14 @@ function Home() {
   };
 
   return (
-    <div className="w-full  bg-slate-900 flex flex-col items-center justify-center">
+    <div className="w-full h-screen  bg-slate-900 flex flex-col items-center justify-center">
       <div className='bg-gray-200 rounded-lg p-6'>
       <div>
-            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Join Room</label>
-            <input type="text" value={remotePeerIdValue} onChange={e => setRemotePeerIdValue(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Room ID" required />
+            <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Join Room</label>
+            <input type="text" value={remotePeerIdValue} onChange={e => setRemotePeerIdValue(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Room ID" required />
         </div>
-        <button type="button" onClick={() => call(remotePeerIdValue)} class="text-white w-full mt-2 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Join</button>
-        <button type="button" onClick={() => call(remotePeerIdValue)} class="text-white w-full mt-2 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Copy </button>
+        <button type="button" onClick={() => call(remotePeerIdValue)} className="text-white w-full mt-2 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Join</button>
+        <button type="button" onClick={() => call(remotePeerIdValue)} className="text-white w-full mt-2 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Copy </button>
         <p className='text-slate-600 text-sm font-semibold'>Your Room ID:</p>
         <p className='text-slate-400 text-xs'>{peerId}</p>
       </div>
